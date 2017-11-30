cd18a6b3b9894873e2edff6399ec97df616327f7
JarvisZhao <ztc_jarvis@hotmail.com>
2017-11-29 23:13:50 -0500

    Finish Resnet implementation?

--- resnet.py
+++ resnet.py
@@ -24,24 +24,13 @@ from keras.layers import Add
 from keras.layers.normalization import BatchNormalization as BN
 from keras.regularizers import l2
 
-
+from keras.callbacks import TensorBoard
 
 
 
 # In[4]:
 
-class Hyperparameters(object):
-    pass
-
-def GenHyperparameters():
-    hp = Hyperparameters() 
-    hp.depth1 = np.random.choice([4])
-    hp.depth2 = np.random.choice([2])
-    hp.filters = np.random.choice([32,64])
-    hp.nodes = np.random.choice([16,64,128])
-    hp.stopping = np.random.choice([5])
-    hp.dropout = np.random.choice([0,0.2])
-    return hp    
+  
 
 
 # In[13]:
@@ -49,61 +38,61 @@ def GenHyperparameters():
 def bn_relu_conv_block(filters,kernel_size,strides=(1,1),padding='same'):
     def f(input):
         input = BN()(input)
-        input = Activation('relu')(norm)
+        input = Activation('relu')(input)
         return Conv2D(filters = filters,kernel_size=kernel_size,strides=strides,padding=padding)(input)
     return f
 
-def normal_residual_unit(filter,is_first_block_of_first_layer=False):
-	def f(input):
+def normal_residual_unit(filter,strides=(1,1),is_first_block_of_first_layer=False):
+    def f(input):
 
-    	shortcut = inputs
-    	if is_first_block_of_first_layer:
-    		conv1 = Conv2D(filters=filter,kernel_size=(3,3),strides=(1,1),padding='same' )(input)
-    	else:
-    		conv1 = bn_relu_conv_block(filters=filter,kernel_size=(3,3))(input)
-    	conv2 = bn_relu_conv_block(filters = filter,kernel_size=(3,3))(conv1)
-    	shortcut = Conv2D(filters=filter,kernel_size=(3,3))(shortcut)
-    	added = Add()([conv2,shortcut])
-    	return added
+        shortcut = input
+        if is_first_block_of_first_layer:
+            conv1 = Conv2D(filters=filter,kernel_size=(3,3),strides=(1,1),padding='same' )(input)
+        else:
+            conv1 = bn_relu_conv_block(filters=filter,kernel_size=(3,3))(input)
+        conv2 = bn_relu_conv_block(filters = filter,kernel_size=(3,3),strides=strides)(conv1)
+        shortcut = Conv2D(filters=filter,kernel_size=(1,1),strides=strides,padding='same')(shortcut)
+        added = Add()([conv2,shortcut])
+        return added
     return f
 
-def bottleneck_residual_unit(filter,is_first_block_of_first_layer=False):
+def bottleneck_residual_unit(filter,strides=(1,1),is_first_block_of_first_layer=False):
     def f(input):
-    	shortcut = input
-    	if is_first_block_of_first_layer:
-    		conv_1_1 = Conv2D(filters = filter,kernel_size=1,strides=(1,1),padding='same')(input)
-	    else:
-	    	conv_1_1 = bn_relu_conv_block(filters = filter,kernel_size=(1,1),strides=(1,1),padding='same')(input)
-	    conv_3_3 =  bn_relu_conv_block(filters = filter,kernel_size=(3,3))(conv_1_1)
-	    res = bn_relu_conv_block(filters=filter*4,kernel_size=(1,1))(conv_3_3)
-	    shortcut = Conv2D(filters = filter*4,kernel_size=1)(shortcut)
-	    added = Add()([res,shortcut])
-	    return added
+        shortcut = input
+        if is_first_block_of_first_layer:
+            conv_1_1 = Conv2D(filters = filter,kernel_size=1,strides=(1,1),padding='same')(input)
+        else:
+            conv_1_1 = bn_relu_conv_block(filters = filter,kernel_size=(1,1),strides=(1,1),padding='same')(input)
+        conv_3_3 =  bn_relu_conv_block(filters = filter,kernel_size=(3,3))(conv_1_1)
+        res = bn_relu_conv_block(filters=filter*4,kernel_size=(1,1),strides=strides)(conv_3_3)
+        shortcut = Conv2D(filters = filter*4,kernel_size=1,strides=strides)(shortcut)
+        added = Add()([res,shortcut])
+        return added
     return f
 
 
 def bn_relu_deconv_block(filters,kernel_size,strides=(1,1),padding='same'):
     def f(input):
         input = BN()(input)
-        input = Activation('relu')(norm)
-        return Conv2Transpose(filters = filters,kernel_size=kernel_size,strides=strides,padding=padding)(input)
+        input = Activation('relu')(input)
+        return Conv2DTranspose(filters = filters,kernel_size=kernel_size,strides=strides,padding='same')(input)
     return f
 
-def de_normal_deresidual_unit(filter,is_first_block_of_first_layer=False):
+def de_normal_residual_unit(filter,strides=(1,1),is_first_block_of_first_layer=False):
     def f(input):
 
-        shortcut = inputs
+        shortcut = input
         if is_first_block_of_first_layer:
             conv1 = Conv2DTransposeTranspose(filters=filter,kernel_size=(3,3),strides=(1,1),padding='same' )(input)
         else:
             conv1 = bn_relu_deconv_block(filters=filter,kernel_size=(3,3))(input)
-        conv2 = bn_relu_deconv_block(filters = filter,kernel_size=(3,3))(conv1)
-        shortcut = Conv2DTranspose(filters=filter,kernel_size=(3,3))(shortcut)
+        conv2 = bn_relu_deconv_block(filters = filter,kernel_size=(3,3),strides=strides)(conv1)
+        shortcut = Conv2DTranspose(filters=filter,kernel_size=(1,1),strides=strides)(shortcut)
         added = Add()([conv2,shortcut])
         return added
     return f
 
-def de_bottleneck_residual_unit(filter,is_first_block_of_first_layer=False):
+def de_bottleneck_residual_unit(filter,strides=(1,1),is_first_block_of_first_layer=False):
     def f(input):
         shortcut = input
         if is_first_block_of_first_layer:
@@ -111,8 +100,8 @@ def de_bottleneck_residual_unit(filter,is_first_block_of_first_layer=False):
         else:
             conv_1_1 = bn_relu_deconv_block(filters = filter,kernel_size=(1,1),strides=(1,1),padding='same')(input)
         conv_3_3 =  bn_relu_deconv_block(filters = filter,kernel_size=(3,3))(conv_1_1)
-        res = bn_relu_deconv_block(filters=filter*4,kernel_size=(1,1))(conv_3_3)
-        shortcut = Conv2DTranspose(filters = filter*4,kernel_size=1)(shortcut)
+        res = bn_relu_deconv_block(filters=filter*4,kernel_size=(1,1),strides=strides)(conv_3_3)
+        shortcut = Conv2DTranspose(filters = filter*4,kernel_size=1,strides=strides)(shortcut)
         added = Add()([res,shortcut])
         return added
     return f
@@ -121,32 +110,55 @@ def de_bottleneck_residual_unit(filter,is_first_block_of_first_layer=False):
 
 
 
-def BuildModel(input_shape,num_outputs,block,structure):
+def BuildModel(input_shape,init_filters,blockfn,structure):
     input = Input(shape = input_shape)
-    conv1 = Conv2D(filters=64, kernel_size=(7,7),strides=(2,2))(input)
-    pool1 = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="same")(conv1)
-    for n in structure:
-        for k in range(n):
+    conv1 = Conv2D(filters=init_filters, kernel_size=(7,7),strides=(2,2))(input)
+    conv1_bn = BN()(conv1)
+    conv1_bn_relu = Activation('relu')(conv1_bn)
+    pool1 = MaxPooling2D(pool_size=(3,3),strides=(2,2),padding="same")(conv1_bn_relu)
+    filters = init_filters
+    block = pool1
+    print(structure)
+    for n in range(len(structure)):
+        for k in range(structure[n]):
+            if not k==structure[n]-1:
+                if blockfn == 'basic':
+                    
+                    block = normal_residual_unit(filter=filters,strides=(1,1),is_first_block_of_first_layer=(n==0 and k==0))(block)
+                elif blockfn == 'bottleneck':
+                    block = bottleneck_residual_unit(filter=filters,strides=(1,1),is_first_block_of_first_layer=(n==0 and k==0))(block)
+            else:
+                if blockfn == 'basic':
+                    
+                    block = normal_residual_unit(filter=filters,strides=(2,2),is_first_block_of_first_layer=(n==0 and k==0))(block)
+                elif blockfn == 'bottleneck':
+                    block = bottleneck_residual_unit(filter=filters,strides=(2,2),is_first_block_of_first_layer=(n==0 and k==0))(block)
+        filters *= 2
 
-
-    filters = hp.filters
-    input_flag = True
+    for n in range(len(structure)-1,-1,-1):
+        for k in range(structure[n]):
+            if not k== structure[n]-1:
+                if blockfn == 'basic':
+                    block = de_normal_residual_unit(filters)(block)
+                elif blockfn == 'bottleneck':
+                    block = de_bottleneck_residual_unit(filters)(block)
+            else:
+                if blockfn == 'basic':
+                    block = de_normal_residual_unit(filters,strides=(2,2))(block)
+                elif blockfn == 'bottleneck':
+                    block = de_bottleneck_residual_unit(filters,strides=(2,2))(block)
+        #block = Conv2DTranspose(filters=filters,kernel_size=3,strides=(2,2))(block)
+        filters//=2
+    block = bn_relu_deconv_block(filters=filters,kernel_size=2,strides=(2,2))(block)
+    block = Conv2DTranspose(filters=3,kernel_size=1,strides=(2,2))(block)
     
-    out = inputs
-    for k in np.arange(hp.depth1)+1:
-        out = BuildModule(out,depth=hp.depth2,
-                filters=filters)
-        input_flag=False
-        filters*=2
-    out = Flatten()(out)
-    out = Dense(hp.nodes)(out)
-    out = BatchNormalization()(out)
-    out = Activation('relu')(out)
-    out = Dropout(hp.dropout)(out)
-    out = Dense(10)(out)
-    out = Activation('softmax')(out)
+    
+    block = Flatten()(block)
+    # for k in f_c:
+    #     block = Dense(k)(block)
+    # block = Dense(384*384*3)(block)
     # < code ommitted >
-    model = Model(inputs=inputs,outputs=out)
+    model = Model(inputs=input,outputs=block)
     
     return model
 
@@ -158,26 +170,26 @@ EPOCHS = 100
 BATCH  = 1000
 TRIALS = 10
 
-hp = GenHyperparameters()
 
-model = BuildModel(hp)
+
+model = BuildModel(input_shape=[384,384,3],init_filters=64,blockfn='bottleneck',structure=[2,2,2,2])
 cb=TensorBoard(log_dir='Resnet', histogram_freq=0,  
       write_graph=True, write_images=True)
 model.compile(loss='categorical_crossentropy',optimizer='Adam',metrics=['accuracy'])
 print(model.summary())
-time_start = time.time()
-hist = model.fit(x_train[:N,:],y_train[:N,:],
-    batch_size=BATCH,
-    epochs=EPOCHS,
-    validation_split=0.2,
-    #
-    callbacks=[EarlyStopping(patience=hp.stopping),cb]
-    )
-time_stop = time.time()
-time_elapsed = (time_stop - time_start)/60
-train_err = 1 - hist.history['acc'][-1]
-val_err = 1 - hist.history['val_acc'][-1]
-test_acc = model.evaluate(x_test,y_test,batch_size=BATCH,verbose=0)
-test_err = 1 - test_acc[1]
+# time_start = time.time()
+# hist = model.fit(x_train[:N,:],y_train[:N,:],
+#     batch_size=BATCH,
+#     epochs=EPOCHS,
+#     validation_split=0.2,
+#     #
+#     callbacks=[EarlyStopping(patience=hp.stopping),cb]
+#     )
+# time_stop = time.time()
+# time_elapsed = (time_stop - time_start)/60
+# train_err = 1 - hist.history['acc'][-1]
+# val_err = 1 - hist.history['val_acc'][-1]
+# test_acc = model.evaluate(x_test,y_test,batch_size=BATCH,verbose=0)
+# test_err = 1 - test_acc[1]
 
 
